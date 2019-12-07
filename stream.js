69f42290eaf2c79c5b474c81c651f68418d469bb
Kaki Ryan <kakiryan6@gmail.com>
2019-12-06 21:09:01 -0500

    stream page can play a random mp3  lol

---
+++ stream.js
@@ -0,0 +1,22 @@
+let sound = new Howl({
+    src: ['sounds/bensound-summer.mp3']
+});
+
+let play = () => {
+    sound.play();
+};
+
+let pause = () => {
+    sound.pause();
+};
+
+let playButton = document.getElementById('playButton');
+
+async function renderPage() {
+    let page = $('body');
+    page.on('click', '#playButton', play);
+    page.on('click', '#pauseButton', pause);
+}
+
+
+$(document).ready(renderPage());
