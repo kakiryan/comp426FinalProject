a6f6ed63174ead9fb1da86c6e5ec655d77ef1b85
Everett Desern <edesern@live.unc.edu>
2019-12-08 16:17:49 -0500

    scriptingggg

---
+++ userprofilescript.js
@@ -0,0 +1,18 @@
+const handleEditBio = function(event) {
+    let html = `<textarea class="textarea" placeholder="e.g. Hello world"></textarea>`
+$('.bio').append()
+}
+
+
+
+
+$(function() {
+    $('#editBio').on('click', handleEditBio);
+
+
+
+
+
+
+
+});
