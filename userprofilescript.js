1f0e5730e41060327ac740226a7038b53988eaa1
Everett Desern <edesern@live.unc.edu>
2019-12-08 16:40:37 -0500

    users can edit their bio now

--- userprofilescript.js
+++ userprofilescript.js
@@ -1,6 +1,16 @@
 const handleEditBio = function(event) {
-    let html = `<textarea class="textarea" placeholder="e.g. Hello world"></textarea>`
-$('.bio').append()
+    let html = `<textarea class="textarea" id="changes" placeholder="e.g. Hello! My name is Bob."></textarea>`;
+    let html2 = `<input class="button is-dark is-small" id="editBioo" value = "Submit Changes" />`;
+$('.bio').append(html);
+$('.bio').append(html2);
+console.log("post");
+$('#editBioo').on('click', handleSubmit);
+}
+
+const handleSubmit = function(event) {
+    console.log("hereee");
+    let t = $('#changes').val();
+    $('.bio').text(t);
 }
 
 
@@ -8,11 +18,7 @@ $('.bio').append()
 
 $(function() {
     $('#editBio').on('click', handleEditBio);
-
-
-
-
-
-
+    console.log("pre");
+    $('#changes').on('click', handleSubmit);
 
 });
