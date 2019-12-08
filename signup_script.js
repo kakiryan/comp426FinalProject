bfaf6d59a979b20f7cdab81f86459a1f9840fafa
Everett Desern <edesern@live.unc.edu>
2019-12-08 16:28:36 -0500

    s

--- signup_script.js
+++ signup_script.js
@@ -9,7 +9,6 @@ async function renderPage() {
         let user = e.target.user.value;
         let pass = e.target.pass.value;
         let name = e.target.name.value;
-        console.log(user);
         const response =  axios({
             method: 'POST',
             url: 'http://localhost:3000/account/create',
