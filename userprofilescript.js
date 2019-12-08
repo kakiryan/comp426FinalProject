2326b8f9ff23753dc32f053d621ffd208ba816b4
Everett Desern <edesern@live.unc.edu>
2019-12-08 17:07:54 -0500

    stuffffff

--- userprofilescript.js
+++ userprofilescript.js
@@ -13,12 +13,54 @@ const handleSubmit = function(event) {
     $('.bio').text(t);
 }
 
+const handleEditPic = function(event) {
+    let html = `<div class="file is-warning" id="picture">
+    <label class="file-label">
+      <input class="file-input" type="file" name="resume">
+      <span class="file-cta">
+        <span class="file-icon">
+          <i class="fas fa-upload"></i>
+        </span>
+        <span class="file-label">
+          Choose a file…
+        </span>
+      </span>
+      <span class="file-name">
+      No File Uploaded
+    </span>
+    </label>
+  </div>`
 
+  let html2 = `<input class="button is-dark is-small" id="editBioo2" value = "Submit Picture" />`;
+
+  $('.pic').append(html);
+  $('.pic').append(html2);
+  const fileInput = document.querySelector('#picture input[type=file]');
+  fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+      const fileName = document.querySelector('#picture .file-name');
+      fileName.textContent = fileInput.files[0].name;
+    }
+  }
+
+    $('#editBioo2').on('click', handleSubmitPic);
+
+}
+
+const handleSubmitPic = function(event) {
+    let t = $('#changes').val();
+    $('.bio').text(t);
+
+    let image = fileName.textContent;
+  
+      $('.pic').text(image); 
+}
 
 
 $(function() {
     $('#editBio').on('click', handleEditBio);
     console.log("pre");
     $('#changes').on('click', handleSubmit);
+    $('#editPic').on('click', handleEditPic);
 
 });
