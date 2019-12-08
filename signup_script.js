4f37787b338dd237a02d61e5e3224c10a50846b1
Kaki Ryan <kakiryan6@gmail.com>
2019-12-08 14:09:49 -0500

    fix sign up form

--- signup_script.js
+++ signup_script.js
@@ -3,10 +3,10 @@ async function handleSubmitButtonPress(e) {
     e.preventDefault();
     console.log('hey')
     console.log(e.target);
-    //let name = e.target.name.value
-    //let pass = e.target.pass.value
-    let name = "Test1";
-    let pass = "1234"
+    let name = e.target.name.value
+    let pass = e.target.pass.value
+    // let name = "Test2";
+    // let pass = "1234"
     // await createAccount({name,  pass});
     const response = await axios({
         method: 'POST',
@@ -25,8 +25,28 @@ async function handleSubmitButtonPress(e) {
 async function renderPage() {
     let page = $('body');
     console.log('here');
-    $("#submitButton").click(handleSubmitButtonPress);
+    // $("#submitButton").click(handleSubmitButtonPress);
+    $("#signUpForm").submit(function (e) {
+        event.preventDefault();
+        console.log(e.target);
+        let user = e.target.user.value
+        let pass = e.target.pass.value
+        let name = e.target.name.value;
+        // let name = "Test2";
+        // let pass = "1234"
+        // await createAccount({name,  pass});
+        const response =  axios({
+            method: 'POST',
+            url: 'http://localhost:3000/account/create',
+            data: {
+                "name": user,
+                "pass": pass,
+                "data": { "firstname": name,
+                }
+            }
+        });
+        console.log(response);
+        window.location.href = "signin.html";
+    });
 }
-
-
 $(document).ready(renderPage());
