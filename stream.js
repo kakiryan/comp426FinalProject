7a30c6590db3b3571aadb05808cde8c5c1c21c65
Alex <aproca@live.unc.edu>
2019-12-09 18:13:04 -0500

    songs

--- stream.js
+++ stream.js
@@ -1,13 +1,13 @@
 //import largeFunct from "./stream_player.js";
-
-/*$(function() {
+/*
+$(function() {
     var audioPlayer = document.querySelector('#song1');
     let strm1 = new stream_player(audioPlayer);
     strm1.doShit();
     let audioPlayer2 = document.querySelector('#song2');
     let strm2 = new stream_player(audioPlayer2);
     strm2.doShit();
-}); */
+}); 
 /*
 export default class streamer {
     constructor() {
