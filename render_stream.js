31b9f8019007116c0c7686fa021b66a5de244cf8
Alex <aproca@live.unc.edu>
2019-12-08 23:49:40 -0500

    fuck my life

---
+++ render_stream.js
@@ -0,0 +1,4 @@
+import streamer from "./stream.js";
+
+let thestream = new streamer();
+
--- stream.js
+++ stream.js
@@ -1,42 +1,21 @@
 import stream_player from "./stream_player.js";
 
-$(function() {
+/*$(function() {
     var audioPlayer = document.querySelector('#song1');
     let strm1 = new stream_player(audioPlayer);
     strm1.doShit();
     let audioPlayer2 = document.querySelector('#song2');
     let strm2 = new stream_player(audioPlayer2);
     strm2.doShit();
-});
+}); */
 
-/*
-var audioPlayer = document.querySelector('#song1');
-let strm1 = new stream_player(audioPlayer);
-strm1.doShit();
-
-let sound = new Howl({
-    src: ['sounds/bensound-summer.mp3'],
-    loop: false
-});
-
-
-let play = () => {
-    sound.play();
-};
-
-let pause = () => {
-    sound.pause();
-};
-
-
-let playButton = document.getElementById('playButton');
-*/
-/*
-async function renderPage() {
-    let page = $('body');
-    page.on('click', '#playButton', play);
-    page.on('click', '#pauseButton', pause);
+export default class streamer {
+    constructor() {
+        let audioPlayer = document.querySelector('#song1');
+        let strm1 = new stream_player(audioPlayer);
+        //strm1.doShit();
+        let audioPlayer2 = document.querySelector('#song2');
+        let strm2 = new stream_player(audioPlayer2);
+        //strm2.doShit();
+    }
 }
-
-
-$(document).ready(renderPage()); */
