b831aa97ff8786774fd44a18fb10a93f59c2d4d0
Alex <aproca@live.unc.edu>
2019-12-10 21:24:12 -0500

    uygyug

--- render_stream.js
+++ render_stream.js
@@ -44,6 +44,10 @@ let sound2 = new Howl({
   src: ['sounds/Good Vibrations.mp3']
 });
 
+let sound3 = new Howl({
+  src: ['sounds/Crazy Frog - Axel F (Official Video).mp3']
+});
+
 let play1 = () => {
   sound1.play();
 };
@@ -60,8 +64,17 @@ let pause2 = () => {
   sound2.pause();
 };
 
+let play3 = () => {
+  sound3.play();
+}
+
+let pause3 = () => {
+  sound3.pause();
+}
+
 let playButton1 = document.getElementById('playButton1');
 let playButton2 = document.getElementById('playButton2');
+let playButton3 = document.getElementById('playButton3');
 
 async function renderPage() {
   let page = $('body');
@@ -69,6 +82,8 @@ async function renderPage() {
   page.on('click', '#pauseButton1', pause1);
   page.on('click', '#playButton2', play2);
   page.on('click', '#pauseButton2', pause2);
+  page.on('click', '#playButton3', play3);
+  page.on('click', '#pauseButton3', pause3);
 }
 
 
