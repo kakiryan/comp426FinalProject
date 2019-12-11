2f8d7f66f97508f71cc4e43fd33401b991b63a40
Kaki Ryan <kakiryan6@gmail.com>
2019-12-11 16:17:51 -0500

    fix library

--- library_script.js
+++ library_script.js
@@ -2,7 +2,7 @@ let myStorage = window.localStorage;
 let jwt = myStorage.getItem('jwt');
 let user = myStorage.getItem('user');
 let playFns = [];
-let pauseFns =[];
+let pauseFns = [];
 
 const userRoot = new axios.create({
     baseURL: 'http://localhost:3000/user'
@@ -14,33 +14,33 @@ let sound1 = new Howl({
 
 let sound2 = new Howl({
     src: ['sounds/Good Vibrations.mp3']
-  });
-  
-  let sound3 = new Howl({
+});
+
+let sound3 = new Howl({
     src: ['sounds/Crazy Frog - Axel F (Official Video).mp3']
-  });
+});
 
 let play1 = (sound) => {
     sound1.play();
-  };
-  
-  let pause1 = (sound) => {
+};
+
+let pause1 = (sound) => {
     sound1.pause();
-  };
-  let play2 = () => {
+};
+let play2 = () => {
     sound2.play();
-  };
-  
-  let pause2 = () => {
+};
+
+let pause2 = () => {
     sound2.pause();
-  };
-  let play3 = () => {
+};
+let play3 = () => {
     sound3.play();
-  }
-  
-  let pause3 = () => {
+}
+
+let pause3 = () => {
     sound3.pause();
-  }
+}
 
 async function renderPage() {
     playFns[1] = play1;
@@ -72,43 +72,48 @@ async function renderPage() {
         let y = document.getElementById("libraryHeader");
         console.log(y)
         y.textContent = "You haven't like any songs yet!"
-    } 
+    }
 
-    for (let i = 0; i < likedArray.length; i++) {
-        let songDiv = document.createElement('div');
-        let title = document.createElement('h2');
-        let playBtn = document.createElement('btn')
-        playBtn.id = "playButton"+(i+1);
-        playBtn.className = "button is-warning";
-        playBtn.type = "button";
-        playBtn.textContent = "Play"
-        title.textContent=likedArray[i];
-        title.className = "title"
-        let pauseBtn = document.createElement('btn');
-        pauseBtn.id = "pauseButton"+(i+1);
-        pauseBtn.className = "button is-warning";
-        pauseBtn.type = "button";
-        pauseBtn.textContent = "Pause"
-        let space = document.createElement('span');
-        let br = document.createElement('hr');
-        space.textContent = "  "
-        songDiv.append(title);
-        songDiv.append(playBtn);
-        songDiv.append(space);
-        songDiv.append(pauseBtn);
-        songDiv.append(br);
-        console.log(songDiv);
-        let x;
-        if (title.textContent == "summer") {
-            x = 1;
-        } else if (title.textContent == "goodVibrations") {
-            x =2;
-        } else if (title.textContent = "crazyFrog") {
-            x = 3;
+    for (let i = 0; i < 50; i++) {
+        let index = Object.keys(x.data.result)[i];
+        if (index !== undefined) {
+
+
+            let songDiv = document.createElement('div');
+            let title = document.createElement('h2');
+            let playBtn = document.createElement('btn')
+            playBtn.id = "playButton" + (index);
+            playBtn.className = "button is-warning";
+            playBtn.type = "button";
+            playBtn.textContent = "Play"
+            title.textContent = likedArray[i];
+            title.className = "title"
+            let pauseBtn = document.createElement('btn');
+            pauseBtn.id = "pauseButton" + (index);
+            pauseBtn.className = "button is-warning";
+            pauseBtn.type = "button";
+            pauseBtn.textContent = "Pause"
+            let space = document.createElement('span');
+            let br = document.createElement('hr');
+            space.textContent = "  "
+            songDiv.append(title);
+            songDiv.append(playBtn);
+            songDiv.append(space);
+            songDiv.append(pauseBtn);
+            songDiv.append(br);
+            console.log(songDiv);
+            let x;
+            if (title.textContent == "summer") {
+                x = 1;
+            } else if (title.textContent == "goodVibrations") {
+                x = 2;
+            } else if (title.textContent = "crazyFrog") {
+                x = 3;
+            }
+            $('body').on('click', '#' + playBtn.id, playFns[x])
+            $('body').on('click', '#' + pauseBtn.id, pauseFns[x])
+            $("#songContainer").append(songDiv);
         }
-        $('body').on('click', '#'+playBtn.id, playFns[x])
-        $('body').on('click', '#'+pauseBtn.id, pauseFns[x])
-        $("#songContainer").append(songDiv);
     }
 }
 
